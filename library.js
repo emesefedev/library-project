0bfa94a6c278ca3b14c2de4b119f1acb7f9d246b
emesefe <emesefedev@gmail.com>
2024-08-29 16:53:38 +0200

    Info book (#1)
    * display book info in book card
    
    * complete display book info in book card
    
    * display books
    
    * delete book button

--- library.js
+++ library.js
@@ -1,17 +1,124 @@
-const library = [];
+const books = {};
+let id = 1;
 
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
+
+  this.id = id++
 }
 
 function addBookToLibrary(newBook) {
-  library.push(newBook)
+  books[newBook.id] = newBook
 }
 
-book1 = new Book("El nombre del viento", "El que no escribe", 300, false)
-addBookToLibrary(book1)
+function deleteBookFromLibrary(bookId) {
+  delete books[bookId]
+
+  updateLibrary()
+}
+
+function displayBook(book) {
+  const bookCard = document.createElement("div")
+  bookCard.classList.add("book-card")
+
+  setCardTitle(bookCard, book)
+  setCardAuthor(bookCard, book)
+  setCardPages(bookCard, book)
+
+  setReadToggle(bookCard, book)
+  
+  setDeleteBookButton(bookCard, book)
+
+  library.appendChild(bookCard);
+}
+
+function setCardTitle(bookCard, book) {
+  const cardTitle = document.createElement("p")
+  cardTitle.classList.add("card-title")
+  cardTitle.textContent = book.title
+  bookCard.appendChild(cardTitle);
+}
+
+function setCardAuthor(bookCard, book) {
+  const cardAuthor = document.createElement("p")
+  cardAuthor.classList.add("card-author")
+  cardAuthor.textContent = book.author
+  bookCard.appendChild(cardAuthor);
+}
+
+function setCardPages(bookCard, book) {
+  const cardPages = document.createElement("p")
+  cardPages.classList.add("card-pages")
+  cardPages.textContent = `${book.pages} pages`
+  bookCard.appendChild(cardPages);
+}
+
+function setReadToggle(bookCard, book) {
+  const readToggle = document.createElement("div")
+  readToggle.classList.add("flex", "align-center", "gap-10")
+
+  const readLabel = document.createElement("label")
+  readLabel.htmlFor = `read-${book.id}`
+  readLabel.textContent = "Read"
+  readToggle.appendChild(readLabel);
+
+  const readInput = document.createElement("input")
+  readInput.type = "checkbox"
+  readInput.checked = book.read
+  readInput.id = `read-${book.id}`
+  readInput.name = `read-${book.id}`
+  readToggle.appendChild(readInput);
+
+  bookCard.appendChild(readToggle);
+}
+
+function setDeleteBookButton(bookCard, book) {
+  const deleteBookButton = document.createElement("button")
+  deleteBookButton.classList.add("card-button")
+  deleteBookButton.textContent = "Delete Book"
+  bookCard.appendChild(deleteBookButton);
+
+  deleteBookButton.addEventListener("click", () => {
+    deleteBookFromLibrary(book.id)
+  })
+}
+
+function cleanLibrary() {
+  while (library.firstChild) {
+    library.removeChild(library.lastChild);
+  }
+}
+
+function updateLibrary() {
+  cleanLibrary()
+
+  for (const [id, book] of Object.entries(books)) {
+    displayBook(book)
+  }
+
+  if (library.firstChild == null) {
+    const noBooksText = document.createElement("p")
+    noBooksText.classList.add("text-dark-green")
+    noBooksText.textContent = "No books in library"
+    library.appendChild(noBooksText);
+  }
+}
+
+window.addEventListener("load", () => {
+  book1 = new Book("Los siete maridos de Evelyn Hugo", "Taylor Jenkins Reid", 384, true)
+  book2 = new Book("Entre silencios", "Camila Silva", 276, true)
+  book3 = new Book("Running Close to the Wind", "Alexandra Rowland", 448, false)
+  
+  addBookToLibrary(book1)
+  addBookToLibrary(book2)
+  addBookToLibrary(book3)
+
+  const library = document.querySelector("#library")
+  updateLibrary()
+  
+})
 
 
