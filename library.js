9854be2de7fb5410383cbd160b74bd66bcf6620e
emesefedev <emesefedev@gmail.com>
2024-08-29 13:40:24 +0200

    display book info in book card

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-const library = [];
+const books = [];
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -8,10 +8,61 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(newBook) {
-  library.push(newBook)
+  books.push(newBook)
 }
 
-book1 = new Book("El nombre del viento", "El que no escribe", 300, false)
-addBookToLibrary(book1)
+window.addEventListener("load", () => {
+  console.log("load")
+
+  book1 = new Book("El nombre del viento", "El que no escribe ni aunque lo amenacen", 300, false)
+  addBookToLibrary(book1)
+
+
+  const library = document.querySelector("#library")
+  console.log(library)
+
+  const bookCard = document.createElement("div")
+  bookCard.classList.add("book-card")
+
+  const cardTitle = document.createElement("p")
+  cardTitle.classList.add("card-title")
+  cardTitle.textContent = book1.title
+  bookCard.appendChild(cardTitle);
+
+  const cardAuthor = document.createElement("p")
+  cardAuthor.classList.add("card-author")
+  cardAuthor.textContent = book1.author
+  bookCard.appendChild(cardAuthor);
+
+  const cardPages = document.createElement("p")
+  cardPages.classList.add("card-pages")
+  cardPages.textContent = `${book1.pages} pages`
+  bookCard.appendChild(cardPages);
+
+  const readToggle = document.createElement("div")
+  readToggle.classList.add("flex", "align-center", "gap-10")
+
+  const readLabel = document.createElement("label")
+  readLabel.htmlFor = `read-1`
+  readLabel.textContent = "Read"
+  readToggle.appendChild(readLabel);
+
+  const readInput = document.createElement("input")
+  readInput.type = "checkbox"
+  readInput.id = `read-1`
+  readInput.name = `read-1`
+  readToggle.appendChild(readInput);
+
+  bookCard.appendChild(readToggle);
+
+  const deleteButton = document.createElement("button")
+  deleteButton.classList.add("card-button")
+  deleteButton.textContent = "Delete Book"
+  bookCard.appendChild(deleteButton);
+
+  library.appendChild(bookCard);
+ 
+
+})
 
 
